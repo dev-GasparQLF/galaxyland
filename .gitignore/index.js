0e3f67f2ac4a640ff142b5de6c1c5872aeec3709
Gaspar QLF JavaScript <44068395+dev-GasparQLF@users.noreply.github.com>
2018-11-08 12:28:52 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const settings = require('./settings.json');
+
+var GalaxyLandID = "509778042940424215"
+let prefix = '--'
+
+bot.on('message', message => {
+    if (message.content === prefix + 'ping') {
+        let msgping1 = new Date();
+        let botping = new Date() - message.createdAt;
+        let msgping2 = new Date() - msgping1;
+        let pingembed = new Discord.RichEmbed()
+            .setColor("RANDOM")
+            .addField('API Ping : ', Math.floor(bot.ping) + 'ms')
+            .addField('Bot Ping : ', Math.floor(botping) + 'ms')
+            .addField('Message Ping : ', '~' + Math.round(msgping2) + 'ms')
+            .setTimestamp(new Date())
+            .setFooter(`${message.author.tag}`);          
+        return message.channel.send(pingembed).then(async msg => {
+            msg.edit(pingembed)
+        });
+    }
+})
+
+bot.on('ready', () => {
+    bot.user.setActivity(`${bot.guilds.get(GalaxyLandID).memberCount} membres | discord.me/galaxyland`, { type: 'WATCHING'});
+    console.log('Je suis connecté !')
+})
+
+bot.on('guildMemberAdd', member => {
+    bot.user.setActivity(`${bot.guilds.get(GalaxyLandID).memberCount} membres | discord.me/galaxyland`, { type: 'WATCHING'});
+})
+
+bot.on('guildMemberRemove', member => {
+    bot.user.setActivity(`${bot.guilds.get(GalaxyLandID).memberCount} membres | discord.me/galaxyland`, { type: 'WATCHING'});
+})
+
+bot.login(settings.token)
